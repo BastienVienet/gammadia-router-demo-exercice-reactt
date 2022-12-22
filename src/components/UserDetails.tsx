b0a9326ec2d03a88f04a59528281cbf8c3cc17c2
BastienVienet <bastien.vienet@outlook.com>
2022-12-22 14:41:24 +0100

    Changing h2 to links

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,7 +1,12 @@
+import {useParams} from "react-router-dom";
+
 export const UserDetails = () => {
+    const params = useParams()
+    const userID = params.userId
+    // Same than `const {userId} = useParams()`
     return (
         <div>
-            Details about user
+            Details about user {userID}
         </div>
     )
 }
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,12 +1,23 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet, useSearchParams} from "react-router-dom";
 
 export const Users = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const showActiveUsers = searchParams.get('filter') === 'active'
     return (
-        <div>
-            <h2>User 1</h2>
-            <h2>User 2</h2>
-            <h2>User 3</h2>
-            <Outlet />
-        </div>
+        <>
+            <div>
+                <Link to='1'><h2>User 1</h2></Link>
+                <Link to='2'><h2>User 2</h2></Link>
+                <Link to='3'><h2>User 3</h2></Link>
+                <Outlet/>
+            </div>
+            <div>
+                <button onClick={() => setSearchParams({filter: 'active'})}>Active Users</button>
+                <button onClick={() => setSearchParams({})}>Reset Filter</button>
+            </div>
+            {
+                showActiveUsers ? <h2>Show Active users</h2> : <h2>Showing all users</h2>
+            }
+        </>
     )
 }
